9bbd1f4a1fe9bf8e6f43ba64dc22ddb774049c08
aouelete <aouellette2016@gmail.com>
2015-07-22 15:23:08 -0400

    solarized-light Xresources

--- themes/solarized-light/Xresources/apply.sh
+++ themes/solarized-light/Xresources/apply.sh
@@ -1,68 +1,23 @@
 # source github.com/altercation/solarized
 file=~/.Xresources
 
-#! Common
+apply '/^\*background:/c\*background: #fdf6e3' $file
+apply '/^\*foreground:/c\*foreground: #657b83' $file
 
-##define S_yellow        #b58900
-##define S_orange        #cb4b16
-##define S_red           #dc322f
-##define S_magenta       #d33682
-##define S_violet        #6c71c4
-##define S_blue          #268bd2
-##define S_cyan          #2aa198
-##define S_green         #859900
+apply '/^\*color0:/c\*color0: #eee8d5' $file
+apply '/^\*color1:/c\*color1: #dc322f' $file
+apply '/^\*color2:/c\*color2: #859900' $file
+apply '/^\*color3:/c\*color3: #b58900' $file
+apply '/^\*color4:/c\*color4: #268bd2' $file
+apply '/^\*color5:/c\*color5: #d33682' $file
+apply '/^\*color6:/c\*color6: #2aa198' $file
+apply '/^\*color7:/c\*color7: #073642' $file
+apply '/^\*color9:/c\*color9: #cb4b16' $file
+apply '/^\*color8:/c\*color8: #fdf6e3' $file
+apply '/^\*color10:/c\*color10: #93a1a1' $file
+apply '/^\*color11:/c\*color11: #839496' $file
+apply '/^\*color12:/c\*color12: #657b83' $file
+apply '/^\*color13:/c\*color13: #6c71c4' $file
+apply '/^\*color14:/c\*color14: #586e75' $file
+apply '/^\*color15:/c\*color15: #002b36' $file
 
-
-#! Dark
-
-##define S_base03        #002b36
-##define S_base02        #073642
-##define S_base01        #586e75
-##define S_base00        #657b83
-##define S_base0         #839496
-##define S_base1         #93a1a1
-##define S_base2         #eee8d5
-##define S_base3         #fdf6e3
-
-
-#! Light
-
-#! #define S_base03        #fdf6e3
-#! #define S_base02        #eee8d5
-#! #define S_base01        #93a1a1
-#! #define S_base00        #839496
-#! #define S_base0         #657b83
-#! #define S_base1         #586e75
-#! #define S_base2         #073642
-#! #define S_base3         #002b36
-
-
-#! To only apply colors to your terminal, for example, prefix
-#! the color assignment statement with its name. Example:
-#!
-#! URxvt*background:            S_base03
-
-#*background:              S_base03
-#*foreground:              S_base0
-#*fading:                  40
-#*fadeColor:               S_base03
-#*cursorColor:             S_base1
-#*pointerColorBackground:  S_base01
-#*pointerColorForeground:  S_base1
-
-#*color0:                  S_base02
-#*color1:                  S_red
-#*color2:                  S_green
-#*color3:                  S_yellow
-#*color4:                  S_blue
-#*color5:                  S_magenta
-#*color6:                  S_cyan
-#*color7:                  S_base2
-#*color9:                  S_orange
-#*color8:                  S_base03
-#*color10:                 S_base01
-#*color11:                 S_base00
-#*color12:                 S_base0
-#*color13:                 S_violet
-#*color14:                 S_base1
-#*color15:                 S_base3
