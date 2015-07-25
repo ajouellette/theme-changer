4fe70f49a47324910beeac73cf7c41bf11bb6f8a
aouelete <aouellette2016@gmail.com>
2015-07-25 09:59:32 -0400

    added solarized-dark Xresourcess

---
+++ themes/solarized-dark/Xresources/apply.sh
@@ -0,0 +1,22 @@
+# source github.com/altercation/solarized
+file=~/.Xresources
+
+apply '/^\*background:/c\*background: #002b36' $file
+apply '/^\*foreground:/c\*foreground: #839496' $file
+
+apply '/^\*color0:/c\*color0: #073642' $file
+apply '/^\*color1:/c\*color1: #dc322f' $file
+apply '/^\*color2:/c\*color2: #859900' $file
+apply '/^\*color3:/c\*color3: #b58900' $file
+apply '/^\*color4:/c\*color4: #268bd2' $file
+apply '/^\*color5:/c\*color5: #d33682' $file
+apply '/^\*color6:/c\*color6: #2aa198' $file
+apply '/^\*color7:/c\*color7: #eee8d5' $file
+apply '/^\*color9:/c\*color9: #cb4b16' $file
+apply '/^\*color8:/c\*color8: #002b36' $file
+apply '/^\*color10:/c\*color10: #586e75' $file
+apply '/^\*color11:/c\*color11: #657b83' $file
+apply '/^\*color12:/c\*color12: #839496' $file
+apply '/^\*color13:/c\*color13: #6c71c4' $file
+apply '/^\*color14:/c\*color14: #93a1a1' $file
+apply '/^\*color15:/c\*color15: #fdf6e3' $file
